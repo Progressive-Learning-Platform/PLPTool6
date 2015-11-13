8a3690d549a40c1d67408b2c9758872ac4e51d9a
Zach Moore <zcmoore@asu.edu>
2015-11-12 19:26:22 -0700

    Add ProjectFile implementation

--- src/edu/asu/plp/tool/prototype/model/ProjectFile.java
+++ src/edu/asu/plp/tool/prototype/model/ProjectFile.java
@@ -1,16 +1,36 @@
 package edu.asu.plp.tool.prototype.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class ProjectFile
 {
+	private Project project;
+	private StringProperty nameProperty;
+	
+	public ProjectFile(Project project, String name)
+	{
+		this.project = project;
+		this.nameProperty = new SimpleStringProperty(name);
+	}
+	
 	public String getName()
 	{
-		// TODO Auto-generated method stub return null;
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+		return nameProperty.get();
 	}
-
+	
+	public void setName(String name)
+	{
+		nameProperty.set(name);
+	}
+	
+	public StringProperty nameProperty()
+	{
+		return nameProperty;
+	}
+	
 	public Project getProject()
 	{
-		// TODO Auto-generated method stub return null;
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+		return project;
 	}
 }
