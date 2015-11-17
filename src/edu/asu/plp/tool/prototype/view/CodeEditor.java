b548e8cb973eb76b614704d65aa915bee1d8de0e
Zach Moore <zcmoore@asu.edu>
2015-11-17 12:32:34 -0700

    Format

--- src/edu/asu/plp/tool/prototype/view/CodeEditor.java
+++ src/edu/asu/plp/tool/prototype/view/CodeEditor.java
@@ -1,47 +1,36 @@
 package edu.asu.plp.tool.prototype.view;
 
-import javax.swing.CodeEditorPane;
-import javax.swing.SwingUtilities;
-import javax.*;
+import java.awt.Color;
 
-import javafx.*;
 import javafx.embed.swing.SwingNode;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 
-import java.awt.Color;
-import java.awt.Font;
-
+import javax.swing.CodeEditorPane;
+import javax.swing.SwingUtilities;
 
 public class CodeEditor extends BorderPane
 {
-	
 	private CodeEditorPane textPane;
+	
 	public CodeEditor()
 	{
-		
 		textPane = new CodeEditorPane();
 		
 		SwingNode swingNode = new SwingNode();
-		
 		createSwingContent(swingNode);
-		
 		setCenter(swingNode);
-		
 	}
 	
-	private void createSwingContent(SwingNode swingNode){
-		 SwingUtilities.invokeLater(new Runnable() {
-	            @Override
-	            public void run() {
-	            	
-	               	textPane.setForeground(Color.red);
-	               	textPane.setText("This is a Code Editor");
-	                swingNode.setContent(textPane);
-	                
-	            }
-	        });
-		
+	private void createSwingContent(SwingNode swingNode)
+	{
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run()
+			{
+				textPane.setForeground(Color.red);
+				textPane.setText("This is a Code Editor");
+				swingNode.setContent(textPane);
+			}
+		});
 	}
-	
 }
