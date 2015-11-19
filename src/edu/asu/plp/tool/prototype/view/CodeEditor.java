0b1dc5cbfabd39c549446ec8115aa0abcc97acb1
Zach Moore <zcmoore@asu.edu>
2015-11-18 21:53:54 -0700

    Allow programatic text manipulation
    Accessible text updates with 2-way data binding

--- src/edu/asu/plp/tool/prototype/view/CodeEditor.java
+++ src/edu/asu/plp/tool/prototype/view/CodeEditor.java
@@ -4,6 +4,11 @@ import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableStringValue;
 import javafx.embed.swing.SwingNode;
 import javafx.scene.AccessibleRole;
 import javafx.scene.layout.BorderPane;
@@ -11,12 +16,14 @@ import javafx.scene.layout.BorderPane;
 import javax.swing.CodeEditorPane;
 import javax.swing.SwingUtilities;
 
-public class CodeEditor extends BorderPane
+public class CodeEditor extends BorderPane implements ObservableStringValue
 {
 	private CodeEditorPane textPane;
+	private StringProperty textProperty;
 	
 	public CodeEditor()
 	{
+		textProperty = new SimpleStringProperty();
 		textPane = new CodeEditorPane();
 		textPane.addKeyListener(new UpdateOnKeyPressListener());
 		
@@ -25,12 +32,19 @@ public class CodeEditor extends BorderPane
 		setCenter(swingNode);
 		
 		this.accessibleRoleProperty().set(AccessibleRole.TEXT_AREA);
-		updateAccessibleText();
+		this.textProperty.bindBidirectional(accessibleTextProperty());
 	}
 	
-	private void updateAccessibleText()
+	public void setText(String text)
 	{
-		this.accessibleTextProperty().set(textPane.getText());
+		textPane.setText(text);
+		textProperty.set(text);
+	}
+	
+	private void updateText()
+	{
+		String text = textPane.getText();
+		this.textProperty.set(text);
 	}
 	
 	private void createSwingContent(SwingNode swingNode)
@@ -46,24 +60,65 @@ public class CodeEditor extends BorderPane
 		});
 	}
 	
+	public String getText()
+	{
+		return get();
+	}
+
+	@Override
+	public String get()
+	{
+		return textProperty.get();
+	}
+
+	@Override
+	public String getValue()
+	{
+		return textProperty.getValue();
+	}
+	
+	@Override
+	public void addListener(ChangeListener<? super String> listener)
+	{
+		textProperty.addListener(listener);
+	}
+
+	@Override
+	public void removeListener(ChangeListener<? super String> listener)
+	{
+		textProperty.removeListener(listener);
+	}
+
+	@Override
+	public void addListener(InvalidationListener listener)
+	{
+		textProperty.addListener(listener);
+	}
+
+	@Override
+	public void removeListener(InvalidationListener listener)
+	{
+		textProperty.removeListener(listener);
+	}
+	
 	private class UpdateOnKeyPressListener implements KeyListener
 	{
 		@Override
 		public void keyTyped(KeyEvent arg0)
 		{
-			updateAccessibleText();
+			updateText();
 		}
 		
 		@Override
 		public void keyReleased(KeyEvent arg0)
 		{
-			updateAccessibleText();
+			updateText();
 		}
 		
 		@Override
 		public void keyPressed(KeyEvent arg0)
 		{
-			updateAccessibleText();
+			updateText();
 		}
 	}
 }
