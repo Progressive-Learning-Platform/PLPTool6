dd97f8a87d8c6aa813319cfafb00205aeaae089d
Zach Moore <zcmoore@asu.edu>
2015-11-25 19:10:11 -0700

    Extract anonymous class

--- src/edu/asu/plp/tool/prototype/view/ConsolePane.java
+++ src/edu/asu/plp/tool/prototype/view/ConsolePane.java
@@ -1,11 +1,14 @@
 package edu.asu.plp.tool.prototype.view;
 
+import java.net.URL;
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker.State;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import moore.util.Subroutine;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -13,6 +16,36 @@ import org.w3c.dom.Node;
 
 public class ConsolePane extends BorderPane
 {
+	private static class OnLoadListener implements ChangeListener<State>
+	{
+		private ObservableValue<State> observable;
+		private Subroutine onLoadFunction;
+
+		public static void register(Subroutine function, ObservableValue<State> target)
+		{
+			OnLoadListener listener = new OnLoadListener(function);
+			listener.observable = target;
+			target.addListener(listener);
+		}
+		
+		public OnLoadListener(Subroutine onLoadFunction)
+		{
+			this.onLoadFunction = onLoadFunction;
+		}
+		
+		public void changed(ObservableValue<? extends State> value, State oldState,
+				State newState)
+		{
+			if (newState == State.SUCCEEDED)
+			{
+				onLoadFunction.perform();
+				
+				if (observable != null)
+					observable.removeListener(this);
+			}
+		}
+	}
+	
 	private static final String TEXT_PANE_ID = "textPane";
 	private static final String TEXT_PANE_CLASS = "scrollPane";
 	private static final String CSS_MESSAGE_CLASS = "message";
@@ -25,39 +58,33 @@ public class ConsolePane extends BorderPane
 		WebView view = new WebView();
 		webEngine = view.getEngine();
 		
-		webEngine.getLoadWorker().stateProperty()
-				.addListener(new ChangeListener<State>() {
-					public void changed(ObservableValue<? extends State> value,
-							State oldState, State newState)
-					{
-						if (newState == State.SUCCEEDED)
-						{
-							Document dom = webEngine.getDocument();
-							textPaneElement = dom.createElement("div");
-							textPaneElement.setAttribute("id", TEXT_PANE_ID);
-							textPaneElement.setAttribute("class", TEXT_PANE_CLASS);
-							
-							Node body = dom.getElementsByTagName("body").item(0);
-							body.appendChild(textPaneElement);
-							
-							String cssPath = getClass().getResource(
-									"defaultConsoleStyle.css").toExternalForm();
-							addStylesheet(cssPath);
-							
-							for (int i = 0; i < 25; i++)
-							{
-								println("Test<br/>" + i);
-							}
-							
-							webEngine.getLoadWorker().stateProperty()
-									.removeListener(this);
-						}
-					}
-				});
+		ObservableValue<State> property = webEngine.getLoadWorker().stateProperty();
+		OnLoadListener.register(this::onLoad, property);
+		
 		String content = "<html><head></head><body></body></html>";
 		webEngine.loadContent(content);
 		
 		this.setCenter(view);
+	}
+	
+	private void onLoad()
+	{
+		Document dom = webEngine.getDocument();
+		textPaneElement = dom.createElement("div");
+		textPaneElement.setAttribute("id", TEXT_PANE_ID);
+		textPaneElement.setAttribute("class", TEXT_PANE_CLASS);
+		
+		Node body = dom.getElementsByTagName("body").item(0);
+		body.appendChild(textPaneElement);
+		
+		URL cssURL = getClass().getResource("defaultConsoleStyle.css");
+		String cssPath = cssURL.toExternalForm();
+		addStylesheet(cssPath);
+		
+		for (int i = 0; i < 25; i++)
+		{
+			println("Test<br/>" + i);
+		}
 	}
 	
 	public void println(String message)
