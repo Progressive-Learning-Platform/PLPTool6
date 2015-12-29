cacbb8575ca6dd0a496fc0aa8117cfa12b3af167
Zach Moore <zcmoore@asu.edu>
2015-12-29 09:24:17 -0700

    BiDiOneToMany HashMap documentation

--- src/edu/asu/plp/tool/backend/OrderedBiDirectionalOneToManyHashMap.java
+++ src/edu/asu/plp/tool/backend/OrderedBiDirectionalOneToManyHashMap.java
@@ -6,6 +6,26 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * A hash-table implementation of {@link BiDirectionalOneToManyMap}, with predictable
+ * iteration order. This map maintains a linked list running through it's keys and values
+ * to define the iteration ordering of {@link #keySet()} and {@link #valueSet()}.
+ * <p>
+ * The iteration order over those sets will be the same as the insertion order for the
+ * keys and values respectively. Note that keys or values that are re-entered into the map
+ * will <b>not</b> have an effect on the ordering.
+ * <p>
+ * <b>Note that this implementation is not synchronized.</b> If multiple threads access a
+ * linked hash map concurrently, and at least one of the threads modifies the map
+ * structurally, it <i>must</i> be synchronized externally.
+ * 
+ * @author Moore, Zachary
+ *
+ * @param <K>
+ *            key type
+ * @param <V>
+ *            value type
+ */
 public class OrderedBiDirectionalOneToManyHashMap<K, V> implements
 		BiDirectionalOneToManyMap<K, V>
 {
@@ -36,98 +56,98 @@ public class OrderedBiDirectionalOneToManyHashMap<K, V> implements
 		values.put(value, key);
 		return oldKey;
 	}
-
+	
 	@Override
 	public boolean remove(K key, V value)
 	{
 		// TODO Auto-generated method stub return false;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public List<V> removeKey(K key)
 	{
 		// TODO Auto-generated method stub return null;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public K removeValue(V value)
 	{
 		// TODO Auto-generated method stub return null;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public boolean containsKey(K key)
 	{
 		// TODO Auto-generated method stub return false;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public boolean containsValue(V value)
 	{
 		// TODO Auto-generated method stub return false;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public boolean contains(K key, V value)
 	{
 		// TODO Auto-generated method stub return false;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public K getKey(V value)
 	{
 		// TODO Auto-generated method stub return null;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public List<V> get(K key)
 	{
 		// TODO Auto-generated method stub return null;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public Set<K> keySet()
 	{
 		// TODO Auto-generated method stub return null;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public Set<V> valueSet()
 	{
 		// TODO Auto-generated method stub return null;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public int size()
 	{
 		// TODO Auto-generated method stub return 0;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public int keySize()
 	{
 		// TODO Auto-generated method stub return 0;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public int valueSize()
 	{
 		// TODO Auto-generated method stub return 0;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public boolean clear()
 	{
