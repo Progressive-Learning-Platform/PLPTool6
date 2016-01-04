16850d1e92a5872698bc43cc70c2d7b4376aa6c3
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-01-04 13:19:55 -0700

    Increase in regex performance

--- src/edu/asu/plp/tool/backend/plpisa/assembler/PLPTokenType.java
+++ src/edu/asu/plp/tool/backend/plpisa/assembler/PLPTokenType.java
@@ -7,17 +7,16 @@ public enum PLPTokenType
 {
 	//Foundation
 	//TODO make label colon have nothing after it
-	LABEL_COLON("\\b([a-zA-Z]+([a-zA-Z]+|_)*[a-zA-Z]+):\\B"),
-	LABEL_PLAIN("\\b([a-zA-Z]+([a-zA-Z]+|_+)*[a-zA-Z]+[^:])\\b"),
+	LABEL_COLON("\\b([a-zA-Z]([a-zA-Z]|_|[0-9])*([a-zA-Z]|[0-9])+):\\B"),
+	INSTRUCTION("\\b[a-zA-Z]+\\b"),
+	LABEL_PLAIN("\\b([a-zA-Z]([a-zA-Z]|_|[0-9])*([a-zA-Z]|[0-9])+)\\b"),
 	ADDRESS("\\$([a-zA-Z]|[0-9])+"),
 	COMMENT("#.*$"),
 	COMMA(","),
-	INSTRUCTION("\\b[a-zA-Z]+\\b"),
 	PARENTHESIS_ADDRESS("\\(\\$([a-zA-Z]|[0-9])+\\)"),
 	DIRECTIVE("\\B\\.(.+?)\\b"),
 	STRING("\"(.*?)\""),
 	NUMERIC("\\b((\\d)+|0x([0-9a-fA-F]+))\\b");
-	
 	//TODO include .include, .text, .data
 	
 	private String regex;
