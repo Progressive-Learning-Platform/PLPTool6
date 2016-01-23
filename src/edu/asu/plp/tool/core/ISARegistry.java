50740fad7da834c76a326f6bceaff6842226374d
Zach Moore <zcmoore@asu.edu>
2016-01-23 10:42:52 -0700

    Initial ISARegistry implementation

---
+++ src/edu/asu/plp/tool/core/ISARegistry.java
@@ -0,0 +1,47 @@
+package edu.asu.plp.tool.core;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+import edu.asu.plp.tool.backend.isa.Simulator;
+import edu.asu.plp.tool.backend.plpisa.sim.PLPSimulator;
+
+public class ISARegistry
+{
+	private static ISARegistry globalInstance;
+	private List<ISAModule> registeredModules;
+	
+	public static ISARegistry getGlobalRegistry()
+	{
+		initialize();
+		return globalInstance;
+	}
+	
+	public static void initialize()
+	{
+		if (globalInstance == null)
+			globalInstance = new ISARegistry();
+	}
+	
+	/**
+	 * Private constructor to enforce singleton
+	 */
+	private ISARegistry()
+	{
+		registeredModules = new ArrayList<>();
+		// TODO: Find and load all ISAModules
+		// ISA Modules will be found on the disk, in the PLP directory located in either
+		// the "mods" or "isas" sub-directories.
+		// The modules will be stored in a jar
+		// They will be compiled as a class of type <? extends ISAModule>
+		
+		// TODO: The following is temporary
+		// TODO: Remove and replace with a decoupled solution
+		Function<String, Boolean> supportsProjectType;
+		supportsProjectType = (type) -> type.toLowerCase().startsWith("plp");
+		Simulator simulator = new PLPSimulator();
+		ISAModule plp6Module = new ISAModule(null, simulator, supportsProjectType);
+		registeredModules.add(plp6Module);
+	}
+}
