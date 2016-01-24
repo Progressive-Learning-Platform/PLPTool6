e919a2c54db26314112a8f429b5cbd3e770f32e9
Zach Moore <zcmoore@asu.edu>
2016-01-24 07:49:38 -0700

    Typo on register name

--- src/edu/asu/plp/tool/backend/plpisa/assembler/DisposablePLPAssembler.java
+++ src/edu/asu/plp/tool/backend/plpisa/assembler/DisposablePLPAssembler.java
@@ -738,7 +738,7 @@ public class DisposablePLPAssembler
 		String label = currentToken.getValue();
 		ensureTokenEquality("(call) Expected a label, found: ", PLPTokenType.LABEL_PLAIN);
 		
-		String[] registers = { "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2t", "$t3",
+		String[] registers = { "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3",
 				"$t4", "$t5", "$t6", "$t7", "$t8", "$t9", "$s0", "$s1", "$s2", "$s3",
 				"$s4", "$s5", "$s6", "$s7", "$ra" };
 				
