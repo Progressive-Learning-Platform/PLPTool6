dba3ce12d4e2b2491a5967f49a5980618c67bc5b
Zach Moore <zcmoore@asu.edu>
2016-01-27 20:02:32 -0700

    Housekeeping

--- src/edu/asu/plp/tool/backend/plpisa/PLPAsm.java
+++ src/edu/asu/plp/tool/backend/plpisa/PLPAsm.java
@@ -1,14 +1,8 @@
 package edu.asu.plp.tool.backend.plpisa;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 import edu.asu.plp.tool.backend.isa.ASMFile;
-import edu.asu.plp.tool.backend.util.FileUtil;
 
 public class PLPAsm extends ASMFile
 {
