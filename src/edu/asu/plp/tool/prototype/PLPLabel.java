da38cb60534209ac60de507cc0faa0299e830dbd
Zach Moore <zcmoore@asu.edu>
2016-02-17 12:38:07 -0700

    Label scraping procedure

--- src/edu/asu/plp/tool/prototype/PLPLabel.java
+++ src/edu/asu/plp/tool/prototype/PLPLabel.java
@@ -30,4 +30,42 @@ public class PLPLabel
 	{
 		return lineNumber;
 	}
+	
+	public static List<PLPLabel> scrape(String content)
+	{
+		TokenTypeSet tokenTypes = new TokenTypeSet();
+		tokenTypes.add(new TokenType("[a-zA-Z_\\$][a-zA-Z_\\$0-9]*", "LABEL"));
+		tokenTypes.add(new TokenType("\\n\\r?", "NEW_LINE"));
+		Lexer lexer = new Lexer(tokenTypes);
+		
+		try
+		{
+			List<Token> tokens = lexer.lex(content);
+			List<PLPLabel> labels = new ArrayList<>();
+			int lineNumber = 1;
+			for (Token token : tokens)
+			{
+				if ("NEW_LINE".equals(token.getTypeName()))
+				{
+					lineNumber++;
+				}
+				else if ("LABEL".equals(token.getTypeName()))
+				{
+					String name = token.getValue();
+					PLPLabel label = new PLPLabel(name, lineNumber);
+					labels.add(label);
+				}
+				else
+				{
+					throw new IllegalStateException("Found token: " + token.getTypeName());
+				}
+			}
+			
+			return labels;
+		}
+		catch (LexException e)
+		{
+			throw new IllegalArgumentException("Invalid file", e);
+		}
+	}
 }
