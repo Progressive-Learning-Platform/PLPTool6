a61edb10518d79dc152a271a16024cd003986804
Zach Moore <zcmoore@asu.edu>
2016-02-25 14:55:47 -0700

    Escape CodeEditor.setText() string

--- src/edu/asu/plp/tool/prototype/view/CodeEditor.java
+++ src/edu/asu/plp/tool/prototype/view/CodeEditor.java
@@ -1,5 +1,7 @@
 package edu.asu.plp.tool.prototype.view;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 import javafx.beans.InvalidationListener;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.StringProperty;
@@ -91,6 +93,8 @@ public class CodeEditor extends BorderPane implements ObservableStringValue
 	
 	public void setText(String text)
 	{
+		// TODO: update StringEscapeUtils and use escapeJavaScript() instead
+		text = StringEscapeUtils.escapeJava(text);
 		codeBodyProperty.set(text);
 		System.out.println("Setting value");
 		webView.getEngine().executeScript(
