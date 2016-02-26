14731701deaab525309ba86338780673cf3a1dbd
Zach Moore <zcmoore@asu.edu>
2016-02-26 15:01:45 -0700

    Argument Hierarchy

---
+++ src/edu/asu/plp/tool/backend/plpisa/assembler2/RegisterArgument.java
@@ -0,0 +1,6 @@
+package edu.asu.plp.tool.backend.plpisa.assembler2;
+
+public class RegisterArgument implements Argument
+{
+	
+}
---
+++ src/edu/asu/plp/tool/backend/plpisa/assembler2/Value.java
@@ -0,0 +1,6 @@
+package edu.asu.plp.tool.backend.plpisa.assembler2;
+
+public class Value implements Argument
+{
+	
+}
