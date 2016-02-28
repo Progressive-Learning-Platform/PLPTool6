7878de6b05ce52bf3a9c7616209bd48e2564d89c
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-02-27 18:10:32 -0700

    Application Settings Class
    Can contain saved settings in a saved json object.
    Saved object could contain things like current theme.

---
+++ src/edu/asu/plp/tool/prototype/model/ApplicationSetting.java
@@ -0,0 +1,18 @@
+package edu.asu.plp.tool.prototype.model;
+
+/**
+ * Enumeration for accessing required settings from the application settings file.
+ *
+ * @author Nesbitt, Morgan
+ * Created on 2/23/2016.
+ */
+public enum ApplicationSetting implements Setting
+{
+    RESOURCES_PATH,
+    ACE_PATH,
+    APPLICATION_THEME_BASE_PATH,
+    APPLICATION_THEME_DEFAULT_PATH,
+    LANGUAGES_BASE_PATH,
+    LANGUAGES_MODES_PATH,
+    LANGUAGES_DEFAULT_PATH
+}
