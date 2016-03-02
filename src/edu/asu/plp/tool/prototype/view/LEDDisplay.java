b8484c3a1afc3e4de39400077e5da8a2eacb2936
Zach Moore <zcmoore@asu.edu>
2016-03-02 10:47:58 -0700

    Autosize LEDDisplay

--- src/edu/asu/plp/tool/prototype/view/LEDDisplay.java
+++ src/edu/asu/plp/tool/prototype/view/LEDDisplay.java
@@ -1,6 +1,6 @@
 package edu.asu.plp.tool.prototype.view;
 
-import javafx.scene.Node;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -11,7 +11,7 @@ import javafx.scene.text.TextAlignment;
 
 public class LEDDisplay extends BorderPane
 {
-	private static final int MINIMUM_SIZE = 100;
+	private static final int DEFAULT_SIZE = 100;
 	private static final int NUMBER_OF_LEDS = 8;
 	private static final int FONT_SIZE = 30;
 	private static final String FONT_NAME = "Arial";
@@ -23,26 +23,47 @@ public class LEDDisplay extends BorderPane
 	 * The state of each LED in this panel (on or off) where a bit set to 1 is "on" and a
 	 * bit set to 0 is "off."
 	 * <p>
-	 * Each led corresponds to the bit at {@link #ledStates} >> index, where "index" is the
-	 * index of the desired LED.
+	 * Each led corresponds to the bit at {@link #ledStates} >> index, where "index" is
+	 * the index of the desired LED.
 	 */
 	private int ledStates;
+	private BorderPane[] ledNodes;
 	
 	public LEDDisplay()
 	{
 		GridPane grid = new GridPane();
+		ledNodes = new BorderPane[NUMBER_OF_LEDS];
 		for (int index = 0; index < NUMBER_OF_LEDS; index++)
 		{
 			int ledState = (ledStates >> index) & 1;
 			boolean isLit = (ledState != 0);
-			Node led = createLED(index, isLit);
+			BorderPane led = createLED(index, isLit);
+			ledNodes[index] = led;
 			grid.add(led, index, 0);
 		}
 		
 		setCenter(grid);
+		
+		this.widthProperty().addListener(this::onSizeChange);
 	}
 	
-	private Node createLED(int number, boolean isLit)
+	private void onSizeChange(ObservableValue<? extends Number> value, Number old,
+			Number current)
+	{
+		int size = current.intValue() / NUMBER_OF_LEDS;
+		resizeLEDs(size);
+	}
+	
+	private void resizeLEDs(int size)
+	{
+		for (BorderPane led : ledNodes)
+		{
+			led.setPrefHeight(size);
+			led.setPrefWidth(size);
+		}
+	}
+	
+	private BorderPane createLED(int number, boolean isLit)
 	{
 		String labelText = Integer.toString(number);
 		Label ledLabel = new Label(labelText);
@@ -54,8 +75,8 @@ public class LEDDisplay extends BorderPane
 		style += (isLit) ? LIT_COLOR : UNLIT_COLOR;
 		
 		BorderPane led = new BorderPane();
-		led.setMinHeight(MINIMUM_SIZE);
-		led.setMinWidth(MINIMUM_SIZE);
+		led.setPrefHeight(DEFAULT_SIZE);
+		led.setPrefWidth(DEFAULT_SIZE);
 		led.setStyle(style);
 		led.setCenter(ledLabel);
 		
@@ -71,7 +92,8 @@ public class LEDDisplay extends BorderPane
 	/**
 	 * Alias for {@link #isLEDLit(int)}
 	 * 
-	 * @param ledIndex Index of the LED to retrieve the state of
+	 * @param ledIndex
+	 *            Index of the LED to retrieve the state of
 	 * @return True if the LED is lit, false otherwise
 	 */
 	public boolean getLEDState(int ledIndex)
@@ -84,7 +106,7 @@ public class LEDDisplay extends BorderPane
 		// TODO Auto-generated method stub return false;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	public void toggleLEDState(int ledIndex)
 	{
 		boolean newState = !getLEDState(ledIndex);
