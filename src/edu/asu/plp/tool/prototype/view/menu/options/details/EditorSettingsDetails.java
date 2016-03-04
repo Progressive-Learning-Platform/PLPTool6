0be82b748cca58fd3cdd4802fe08a7f0c6407bee
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-03-03 21:07:33 -0700

    option detail templates for data passing

---
+++ src/edu/asu/plp/tool/prototype/view/menu/options/details/EditorSettingsDetails.java
@@ -0,0 +1,10 @@
+package edu.asu.plp.tool.prototype.view.menu.options.details;
+
+/**
+ * @author Nesbitt, Morgan on 2/27/2016.
+ */
+public class EditorSettingsDetails
+{
+	private String editorMode;
+	private String editorTheme;
+}
---
+++ src/edu/asu/plp/tool/prototype/view/menu/options/details/SimulatorSettingsDetails.java
@@ -0,0 +1,8 @@
+package edu.asu.plp.tool.prototype.view.menu.options.details;
+
+/**
+ * @author Nesbitt, Morgan on 2/27/2016.
+ */
+public class SimulatorSettingsDetails
+{
+}
