059e1334d7b4abb92fd821c60485369342ad44ba
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-03-03 21:08:36 -0700

    Options sub panes

---
+++ src/edu/asu/plp/tool/prototype/view/menu/options/sections/EditorSettingsPanel.java
@@ -0,0 +1,10 @@
+package edu.asu.plp.tool.prototype.view.menu.options.sections;
+
+import javafx.scene.layout.BorderPane;
+
+/**
+ * @author Nesbitt, Morgan on 2/27/2016.
+ */
+public class EditorSettingsPanel extends BorderPane
+{
+}
