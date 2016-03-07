654de06355fbacc0be697ea02deab86d098dbecc
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-03-07 13:37:48 -0700

    Add theme selection to application panel

--- src/edu/asu/plp/tool/prototype/view/menu/options/sections/ApplicationSettingsPanel.java
+++ src/edu/asu/plp/tool/prototype/view/menu/options/sections/ApplicationSettingsPanel.java
@@ -1,10 +1,87 @@
 package edu.asu.plp.tool.prototype.view.menu.options.sections;
 
+import edu.asu.plp.tool.prototype.view.menu.options.details.ApplicationSettingDetails;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 /**
  * @author Nesbitt, Morgan on 2/27/2016.
  */
 public class ApplicationSettingsPanel extends BorderPane
 {
+	SingleSelectionModel<String> applicationThemeSelectionModel;
+	SingleSelectionModel<String> editorThemeSelectionModel;
+
+	public ApplicationSettingsPanel( ObservableList<String> applicationThemes, ObservableList<String> editorsThemes )
+	{
+		this(applicationThemes, editorsThemes, ApplicationSettingDetails.DEFAULT);
+	}
+
+	public ApplicationSettingsPanel( ObservableList<String> applicationThemes, ObservableList<String> editorsThemes,
+			ApplicationSettingDetails details )
+	{
+		ApplicationSettingDetails settingDetails = ( details != null ) ? details : ApplicationSettingDetails.DEFAULT;
+
+		VBox settingsColumn = new VBox();
+
+		settingsColumn.getChildren().add(applicationThemeSelection(settingDetails, applicationThemes));
+		settingsColumn.getChildren().add(editorThemeSelection(settingDetails, editorsThemes));
+
+
+		setCenter(settingsColumn);
+	}
+
+	private HBox applicationThemeSelection( ApplicationSettingDetails details,
+			ObservableList<String> applicationThemes )
+	{
+		HBox hbox = new HBox();
+
+		Text themeLabel = new Text("Application Theme: ");
+
+		ObservableList<String> list = applicationThemes;
+
+		ComboBox<String> themes = new ComboBox<>(list);
+		themes.getSelectionModel().select(details.getApplicationTheme());
+
+		applicationThemeSelectionModel = themes.getSelectionModel();
+
+
+		hbox.getChildren().addAll(themeLabel, themes);
+		hbox.setAlignment(Pos.CENTER_LEFT);
+
+		return hbox;
+	}
+
+	private HBox editorThemeSelection( ApplicationSettingDetails details, ObservableList<String> editorsThemes )
+	{
+		HBox hbox = new HBox();
+
+		Text themeLabel = new Text("Editor Theme: ");
+
+		ObservableList<String> list = editorsThemes;
+
+		ComboBox<String> themes = new ComboBox<>(list);
+		themes.getSelectionModel().select(0);
+
+		editorThemeSelectionModel = themes.getSelectionModel();
+
+		hbox.getChildren().addAll(themeLabel, themes);
+		hbox.setAlignment(Pos.CENTER_LEFT);
+
+		return hbox;
+	}
+
+	public ApplicationSettingDetails getResults()
+	{
+		String appTheme = applicationThemeSelectionModel.getSelectedItem();
+		String editorTheme = editorThemeSelectionModel.getSelectedItem();
+
+		return new ApplicationSettingDetails(appTheme, editorTheme);
+	}
 }
