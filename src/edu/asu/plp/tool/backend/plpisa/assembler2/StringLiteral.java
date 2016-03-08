fb8865536a4366eeffe36cc3fe2fa6fa8e8ab0ed
Zach Moore <zcmoore@asu.edu>
2016-03-08 12:44:16 -0700

    PLPValue model placeholders

--- src/edu/asu/plp/tool/backend/plpisa/assembler2/StringLiteral.java
+++ src/edu/asu/plp/tool/backend/plpisa/assembler2/StringLiteral.java
@@ -1,42 +1,46 @@
 package edu.asu.plp.tool.backend.plpisa.assembler2;
 
-public class RegisterArgument implements Argument
+public class StringLiteral implements Argument
 {
 	private String rawValue;
 	
-	public RegisterArgument(String rawValue)
+	public StringLiteral(String rawValue)
 	{
 		this.rawValue = rawValue;
 	}
-	
+
 	@Override
 	public int encode()
 	{
+		// exclude the quotes (first and last character) in value
+		int lastIndex = rawValue.length() - 1;
+		String value = rawValue.substring(1, lastIndex);
+		
 		// TODO Auto-generated method stub return 0;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public boolean isRegister()
 	{
 		// TODO Auto-generated method stub return false;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public boolean isNumberLiteral()
 	{
 		// TODO Auto-generated method stub return false;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public boolean isStringLiteral()
 	{
 		// TODO Auto-generated method stub return false;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public String raw()
 	{
--- src/edu/asu/plp/tool/backend/plpisa/assembler2/Value.java
+++ src/edu/asu/plp/tool/backend/plpisa/assembler2/Value.java
@@ -2,34 +2,39 @@ package edu.asu.plp.tool.backend.plpisa.assembler2;
 
 public class Value implements Argument
 {
+	public Value(int value)
+	{
+		// TODO Auto-generated constructor stub
+	}
+	
 	@Override
 	public int encode()
 	{
 		// TODO Auto-generated method stub return 0;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public boolean isRegister()
 	{
 		// TODO Auto-generated method stub return false;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public boolean isNumberLiteral()
 	{
 		// TODO Auto-generated method stub return false;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public boolean isStringLiteral()
 	{
 		// TODO Auto-generated method stub return false;
 		throw new UnsupportedOperationException("The method is not implemented yet.");
 	}
-
+	
 	@Override
 	public String raw()
 	{
