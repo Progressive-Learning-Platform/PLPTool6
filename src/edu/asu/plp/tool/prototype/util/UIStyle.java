e3524c93c38d373546c985ce1e13c0e27313b1fa
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-03-10 17:47:37 -0700

    UIStyle documentation

--- src/edu/asu/plp/tool/prototype/util/UIStyle.java
+++ src/edu/asu/plp/tool/prototype/util/UIStyle.java
@@ -9,18 +9,39 @@ import javafx.scene.Node;
  */
 public class UIStyle
 {
-	private static final String errorIdentifier = "error";
+	/**
+	 * CSS Class identifier that can be appended to any node that supports it, to activate an error mode.
+	 * <p>
+	 * Currently only the text-input node supports it (Which is contained in TextFields and the ComboBox if editable).
+	 */
+	private static final String ERROR_IDENTIFIER = "error";
 
+	/**
+	 * Adds or removes the error class identifier to a node.
+	 * <p>
+	 * e.g. The normal {@link javafx.scene.control.TextField} contains a .text-input that you can style. A theme can
+	 * style .text-input.error.
+	 * <p>
+	 * If isValid is true it will remove any error styling. If false it will apply said styling.
+	 * <p>
+	 * NOTE: This will only apply error styling to the node, if and only if the current applied theme supports error
+	 * styling for the node.
+	 *
+	 * @param isValid
+	 * 		True if the node has no errors, false otherwise.
+	 * @param node
+	 * 		Node to style
+	 */
 	public static void applyError( boolean isValid, Node node )
 	{
 		if ( isValid )
 		{
-			if ( node.getStyleClass().contains(errorIdentifier) )
-				node.getStyleClass().remove(errorIdentifier);
+			if ( node.getStyleClass().contains(ERROR_IDENTIFIER) )
+				node.getStyleClass().remove(ERROR_IDENTIFIER);
 		} else
 		{
-			if ( !node.getStyleClass().contains(errorIdentifier) )
-				node.getStyleClass().add(errorIdentifier);
+			if ( !node.getStyleClass().contains(ERROR_IDENTIFIER) )
+				node.getStyleClass().add(ERROR_IDENTIFIER);
 		}
 	}
 }
