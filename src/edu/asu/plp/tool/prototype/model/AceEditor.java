cd8cd76f5fe0edd78a9c9ceb42d69e46c2f0123c
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-03-28 19:40:12 -0700

    Editor breakpoints

--- src/edu/asu/plp/tool/prototype/model/AceEditor.java
+++ src/edu/asu/plp/tool/prototype/model/AceEditor.java
@@ -1,17 +1,15 @@
 package edu.asu.plp.tool.prototype.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 import java.io.File;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-
 /**
- * 
  * @author Nesbitt, Morgan
- * 
  */
 public class AceEditor
 {
@@ -27,59 +25,67 @@ public class AceEditor
 			+ "right: 0;" 
 			+ "bottom: 0;"
 			+ "left: 0;" 
-			+ "}" 
+			+ "}"
+			+ ".ace_gutter-cell.ace_breakpoint{ \n"
+			+ "    border-radius: 20px 0px 0px 20px; \n"
+			+ "    box-shadow: 0px 0px 1px 1px red inset; \n"
+			+ "}"
 			+ "</style>" 
 			+ "</head>";
 	//@formatter:on
-	
+
 	private static final String tail = "</body></html>";
-	
+
 	private String currentTheme;
 	private String currentSessionMode;
-	
+
 	private List<EditorRoutine> editorRoutines;
-	
+
 	private StringProperty currentBodyProperty;
-	
+
 	private StringProperty fullPage;
-	
+
 	public AceEditor()
 	{
-		currentTheme = "tomorrow";
+		ApplicationSetting setting = ApplicationSetting.EDITOR_THEME;
+		String editorTheme =
+				SettingUtil.loadRequiredSavedSettingDefaultIfNotPresent(setting);
+
+		currentTheme = editorTheme;
 		currentSessionMode = "plp";
 		currentBodyProperty = new SimpleStringProperty();
 		editorRoutines = new ArrayList<>();
-		
+
 		fullPage = new SimpleStringProperty();
 		buildPage();
 	}
-	
-	public void addCustomJavascriptRoutine(EditorRoutine routine)
+
+	public void addCustomJavascriptRoutine( EditorRoutine routine )
 	{
 		this.editorRoutines.add(routine);
 		buildPage();
 	}
-	
+
 	public StringProperty getPage()
 	{
 		return fullPage;
 	}
-	
+
 	public StringProperty getBodyProperty()
 	{
 		return currentBodyProperty;
 	}
-	
-	public boolean setTheme(String themeName)
+
+	public boolean setTheme( String themeName )
 	{
 		throw new UnsupportedOperationException();
 	}
-	
-	public boolean setSessionMode(String themeName)
+
+	public boolean setSessionMode( String themeName )
 	{
 		throw new UnsupportedOperationException();
 	}
-	
+
 	private void buildPage()
 	{
 		StringBuilder builder = new StringBuilder();
@@ -88,10 +94,10 @@ public class AceEditor
 		builder.append(getAceImportScript());
 		builder.append(getEditorSettings());
 		builder.append(tail);
-		
+
 		fullPage.set(builder.toString());
 	}
-	
+
 	private String getBody()
 	{
 		StringBuilder builder = new StringBuilder();
@@ -100,32 +106,53 @@ public class AceEditor
 		builder.append("</div>");
 		return builder.toString();
 	}
-	
+
 	private String getEditorSettings()
 	{
 		StringBuilder builder = new StringBuilder();
 		builder.append("<script>");
 		builder.append("var editor = ace.edit(\"editor\");");
+		builder.append("editor.on(\"guttermousedown\", function(e){ \n" +
+							   "    var target = e.domEvent.target; \n" +
+							   "    if (target.className.indexOf(\"ace_gutter-cell\") == -1) \n" +
+							   "        return; \n" +
+							   "    if (!editor.isFocused()) \n" +
+							   "        return; \n" +
+							   "    if (e.clientX > 25 + target.getBoundingClientRect().left) \n" +
+							   "        return; \n" +
+							   "\n" +
+							   "    var row = e.getDocumentPosition().row \n" +
+							   "	var session = e.editor.session \n" +
+							   "	var breakPointArray = session.getBreakpoints(); \n" +
+							   "	if(breakPointArray[row] != null) \n" +
+							   "	{ \n" +
+							   "		session.clearBreakpoint(row); \n" +
+							   "		return; \n" +
+							   "	} \n" +
+							   "	session.setBreakpoint(row); \n" +
+							   "    e.stop() \n" +
+							   "}) \n");
+
 		builder.append("editor.setTheme(\"ace/theme/" + currentTheme + "\");");
 		builder.append("editor.getSession().setMode(\"ace/mode/" + currentSessionMode
-				+ "\");");
+							   + "\");");
 		builder.append(getJavascriptRoutines());
 		builder.append("</script>");
-		
+
 		return builder.toString();
 	}
-	
+
 	private String getAceImportScript()
 	{
 		StringBuilder builder = new StringBuilder();
-		
+
 		File expectedAceFilePath = new File("lib/ace/ace.js");
 		String filePath = expectedAceFilePath.getAbsolutePath().toString();
 		try
 		{
 			filePath = expectedAceFilePath.toURI().toURL().toString();
 		}
-		catch (MalformedURLException e)
+		catch ( MalformedURLException e )
 		{
 			e.printStackTrace();
 		}
@@ -133,21 +160,21 @@ public class AceEditor
 		builder.append("text=\"text/javascript\" ");
 		builder.append("charset=\"utf-8\">");
 		builder.append("</script>");
-		
+
 		return builder.toString();
 	}
-	
+
 	private String getJavascriptRoutines()
 	{
-		if (editorRoutines.size() > 0)
+		if ( editorRoutines.size() > 0 )
 		{
 			StringBuilder builder = new StringBuilder();
-			
-			for (EditorRoutine routine : editorRoutines)
+
+			for ( EditorRoutine routine : editorRoutines )
 			{
 				builder.append(routine.get());
 			}
-			
+
 			return builder.toString();
 		}
 		else
