f3fb3268cd08eab40f010f6c140d69c4611273e1
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-04-13 22:41:13 -0700

    add state retrieval from response

--- src/edu/asu/plp/tool/backend/plpisa/sim/stages/events/ExecuteStageStateResponse.java
+++ src/edu/asu/plp/tool/backend/plpisa/sim/stages/events/ExecuteStageStateResponse.java
@@ -4,10 +4,16 @@ import edu.asu.plp.tool.backend.plpisa.sim.stages.state.ExecuteStageState;
 
 public class ExecuteStageStateResponse
 {
+	private ExecuteStageState state;
+	
+	public ExecuteStageStateResponse(ExecuteStageState state)
+	{
+		this.state = state;
+	}
 
 	public ExecuteStageState getExecuteStageState()
 	{
-		return null;
+		return state;
 	}
 
 }
