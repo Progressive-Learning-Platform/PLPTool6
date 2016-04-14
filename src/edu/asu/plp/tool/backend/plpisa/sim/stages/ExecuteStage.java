a1bf1674c64cdf4b2541509b538f594739f349d8
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-04-13 22:46:53 -0700

    register execute event handler

--- src/edu/asu/plp/tool/backend/plpisa/sim/stages/ExecuteStage.java
+++ src/edu/asu/plp/tool/backend/plpisa/sim/stages/ExecuteStage.java
@@ -2,17 +2,28 @@ package edu.asu.plp.tool.backend.plpisa.sim.stages;
 
 import com.google.common.eventbus.EventBus;
 
+import edu.asu.plp.tool.backend.plpisa.sim.stages.events.ExecuteStageStateRequest;
+import edu.asu.plp.tool.backend.plpisa.sim.stages.events.ExecuteStageStateResponse;
 import edu.asu.plp.tool.backend.plpisa.sim.stages.events.InstructionDecodeCompletion;
+import edu.asu.plp.tool.backend.plpisa.sim.stages.state.ExecuteStageState;
 
 public class ExecuteStage implements Stage
 {
 	private EventBus bus;
 	private ExecuteEventHandler eventHandler;
 	
+	private ExecuteStageState state;
+	
 	public ExecuteStage(EventBus simulatorBus)
 	{
 		this.bus = simulatorBus;
-		this.eventHandler =  new ExecuteEventHandler(); 
+		this.eventHandler =  new ExecuteEventHandler();
+		
+		this.bus.register(eventHandler);
+		
+		this.state = new ExecuteStageState();
+		
+		reset();
 	}
 	
 	@Override
