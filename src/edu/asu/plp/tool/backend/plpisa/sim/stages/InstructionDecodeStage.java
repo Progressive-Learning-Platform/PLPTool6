eaad4224d62bdb4f6aeb984280fe6bc51e8f7795
Morgan Nesbitt <nmorgan1214@gmail.com>
2016-04-14 21:04:18 -0700

    switch id to cpu state

--- src/edu/asu/plp/tool/backend/plpisa/sim/stages/InstructionDecodeStage.java
+++ src/edu/asu/plp/tool/backend/plpisa/sim/stages/InstructionDecodeStage.java
@@ -4,43 +4,23 @@ import com.google.common.eventbus.EventBus;
 
 import edu.asu.plp.tool.backend.plpisa.InstructionExtractor;
 import edu.asu.plp.tool.backend.plpisa.PLPInstruction;
-import edu.asu.plp.tool.backend.plpisa.sim.SimulatorFlag;
 import edu.asu.plp.tool.backend.plpisa.sim.stages.events.ExecuteStageStateRequest;
 import edu.asu.plp.tool.backend.plpisa.sim.stages.events.ExecuteStageStateResponse;
 import edu.asu.plp.tool.backend.plpisa.sim.stages.events.InstructionDecodeCompletion;
 import edu.asu.plp.tool.backend.plpisa.sim.stages.events.MemoryStageStateRequest;
 import edu.asu.plp.tool.backend.plpisa.sim.stages.events.MemoryStageStateResponse;
-import edu.asu.plp.tool.backend.plpisa.sim.stages.state.ExecuteStageState;
-import edu.asu.plp.tool.backend.plpisa.sim.stages.state.MemoryStageState;
+import edu.asu.plp.tool.backend.plpisa.sim.stages.state.CpuState;
 
 public class InstructionDecodeStage implements Stage
 {
 	private EventBus bus;
 	private InstructionDecodeEventHandler eventHandler;
 	
-	//TODO move to a state class?
-	
-	private int ifCount;
-	private int idCount;
-	
-	private boolean hot;
-	
-	private boolean bubble;
-	private boolean nextBubble;
-	
-	private long currentInstruction;
-	private long currentInstructionAddress;
-	
-	// Pipeline Registers
-	private long nextInstruction;
-	private long nextInstructionAddress;
-	
-	private long currentCt1Pcplus4;
-	private long nextCt1Pcplus4;
+	private CpuState state;
 	
 	// Get state from other stages thats required (Hard Porting)
-	private ExecuteStageState currentExecuteStageState;
-	private MemoryStageState currentMemoryStageState;
+	private CpuState currentExecuteStageState;
+	private CpuState currentMemoryStageState;
 	
 	public InstructionDecodeStage(EventBus simulatorBus)
 	{
@@ -56,7 +36,7 @@ public class InstructionDecodeStage implements Stage
 	public void evaluate()
 	{
 		InstructionDecodeCompletion executePackage = new InstructionDecodeCompletion();
-		ExecuteStageState postExecuteStageState = new ExecuteStageState();
+		CpuState postExecuteStageState = new CpuState();
 		
 		executePackage.setPostExecuteStageState(postExecuteStageState);
 		
@@ -72,36 +52,36 @@ public class InstructionDecodeStage implements Stage
 		// TODO get from wherever the flag is
 		boolean mem_ex_lw = false;
 		
-		byte opCode = (byte) InstructionExtractor.opcode(currentInstruction);
-		byte funct = (byte) InstructionExtractor.funct(currentInstruction);
+		byte opCode = (byte) InstructionExtractor.opcode(state.currentInstruction);
+		byte funct = (byte) InstructionExtractor.funct(state.currentInstruction);
 		
-		long addressRt = InstructionExtractor.rt(currentInstruction);
-		long addressRs = InstructionExtractor.rs(currentInstruction);
+		long addressRt = InstructionExtractor.rt(state.currentInstruction);
+		long addressRs = InstructionExtractor.rs(state.currentInstruction);
 		
 		long executeStageCurrentInstruction = currentExecuteStageState.currentInstruction;
 
 		// The register being written to by load word
 		long executeRt = InstructionExtractor.rt(executeStageCurrentInstruction);
 		
-		if (hot)
+		if (state.hot)
 		{
-			hot = false;
+			state.hot = false;
 			postExecuteStageState.hot = true;
 		}
 		
-		if (!bubble)
-			idCount++;
+		if (!state.bubble)
+			state.idCount++;
 			
-		postExecuteStageState.nextBubble = bubble;
-		postExecuteStageState.nextInstruction = currentInstruction;
-		postExecuteStageState.nextInstructionAddress = currentInstructionAddress;
+		postExecuteStageState.nextBubble = state.bubble;
+		postExecuteStageState.nextInstruction = state.currentInstruction;
+		postExecuteStageState.nextInstructionAddress = state.currentInstructionAddress;
 		
-		if (currentMemoryStageState.isHot() && mem_ex_lw)
+		if (currentMemoryStageState.hot && mem_ex_lw)
 		{
 			boolean executeEqualsAddressRt = executeRt == addressRt;
 			boolean executeForwardCt1Memread = currentExecuteStageState.forwardCt1Memread == 1;
 			boolean isCurrentInstructionNotStoreWord = InstructionExtractor.opcode(
-					currentInstruction) != PLPInstruction.STORE_WORD.getByteCode();
+					state.currentInstruction) != PLPInstruction.STORE_WORD.getByteCode();
 					
 			if (executeEqualsAddressRt && (addressRt != 0) && executeForwardCt1Memread)
 			{
@@ -123,7 +103,7 @@ public class InstructionDecodeStage implements Stage
 		// long rs = (addressRs == 0) ? 0 : (Long) memoryModule.read(addressRs);
 		// executePackage.setNextDataRs(rs);
 		
-		long immediateField = InstructionExtractor.imm(currentInstruction);
+		long immediateField = InstructionExtractor.imm(state.currentInstruction);
 		
 		boolean isNotAndImmediate = opCode != PLPInstruction.AND_IMMEDIATE.getByteCode();
 		boolean isNotOrImmediate = opCode != PLPInstruction.OR_IMMEDIATE.getByteCode();
@@ -139,18 +119,18 @@ public class InstructionDecodeStage implements Stage
 		}
 		
 		postExecuteStageState.nextCt1RdAddress = InstructionExtractor
-				.rd(currentInstruction); // rd
+				.rd(state.currentInstruction); // rd
 		postExecuteStageState.nextCt1RtAddress = addressRt;
 		
-		postExecuteStageState.nextCt1AluOp = currentInstruction;
+		postExecuteStageState.nextCt1AluOp = state.currentInstruction;
 		
-		postExecuteStageState.nextForwardCt1LinkAddress = currentCt1Pcplus4 + 4;
+		postExecuteStageState.nextForwardCt1LinkAddress = state.ct1Pcplus4 + 4;
 		
 		executePackage.clearLogic();
 		
 		if (opCode != PLPInstruction.SHIFT_LEFT_LOGICAL.getByteCode())
 		{
-			switch (InstructionExtractor.instructionType(currentInstruction))
+			switch (InstructionExtractor.instructionType(state.currentInstruction))
 			{
 				case 3: // beq and bne
 					postExecuteStageState.nextCt1Branch = 1;
@@ -175,7 +155,7 @@ public class InstructionDecodeStage implements Stage
 					break;
 				case 7: // j and jal
 					postExecuteStageState.nextCt1Jump = 1;
-					if (InstructionExtractor.mnemonic(currentInstruction)
+					if (InstructionExtractor.mnemonic(state.currentInstruction)
 							.equals(PLPInstruction.JUMP_AND_LINK.getMnemonic()))
 					{
 						postExecuteStageState.nextCt1Regdest = 1;
@@ -190,7 +170,7 @@ public class InstructionDecodeStage implements Stage
 		}
 		else
 		{
-			switch (InstructionExtractor.instructionType(currentInstruction))
+			switch (InstructionExtractor.instructionType(state.currentInstruction))
 			{
 				case 0: // r-types
 				case 1: // shifts
@@ -212,7 +192,7 @@ public class InstructionDecodeStage implements Stage
 			}
 		}
 		
-		long nextCt1BranchTarget = currentCt1Pcplus4
+		long nextCt1BranchTarget = state.ct1Pcplus4
 				+ ((short) postExecuteStageState.nextDataImmediateSignExtended << 2);
 		postExecuteStageState.nextCt1BranchTarget = nextCt1BranchTarget;
 		
@@ -222,10 +202,10 @@ public class InstructionDecodeStage implements Stage
 	@Override
 	public void clock()
 	{
-		bubble = nextBubble;
-		currentCt1Pcplus4 = nextCt1Pcplus4;
-		currentInstruction = nextInstruction;
-		currentInstructionAddress = nextInstructionAddress;
+		state.bubble = state.nextBubble;
+		state.ct1Pcplus4 = state.nextCt1Pcplus4;
+		state.currentInstruction = state.nextInstruction;
+		state.currentInstructionAddress = state.nextInstructionAddress;
 		
 		currentExecuteStageState = null;
 		currentMemoryStageState = null;
@@ -234,26 +214,26 @@ public class InstructionDecodeStage implements Stage
 	@Override
 	public void printVariables()
 	{
-		prettyPrintVariables("Instruction Decode Variables", currentInstruction,
-				currentInstructionAddress, currentCt1Pcplus4);
+		prettyPrintVariables("Instruction Decode Variables", state.currentInstruction,
+				state.currentInstructionAddress, state.ct1Pcplus4);
 	}
 	
 	@Override
 	public void printNextVariables()
 	{
-		prettyPrintVariables("Instruction Decode Next Variables", nextInstruction,
-				nextInstructionAddress, nextCt1Pcplus4);
+		prettyPrintVariables("Instruction Decode Next Variables", state.nextInstruction,
+				state.nextInstructionAddress, state.nextCt1Pcplus4);
 	}
 	
 	@Override
 	public String printInstruction()
 	{
-		String formattedInstructionAddress = (currentInstructionAddress == -1 || bubble)
-				? "--------" : String.format("08x", currentInstructionAddress);
+		String formattedInstructionAddress = (state.currentInstructionAddress == -1 || state.bubble)
+				? "--------" : String.format("08x", state.currentInstructionAddress);
 				
 		// TODO add MIPSInstr format like ability
 		String instruction = String.format("%s %s %s %08x", "Instruction Decode:",
-				formattedInstructionAddress, "Instruction:", currentInstruction);
+				formattedInstructionAddress, "Instruction:", state.currentInstruction);
 				
 		return instruction;
 	}
@@ -267,7 +247,7 @@ public class InstructionDecodeStage implements Stage
 		String instructionFormatted = String.format("%-25s %08x %s", "\tinstruction",
 				instruction);
 				
-		String instructionAddressTemp = (currentInstructionAddress == -1) ? "--------"
+		String instructionAddressTemp = (state.currentInstructionAddress == -1) ? "--------"
 				: String.format("%08x", instructionAddress);
 		String instructionAddressFormatted = String.format("%-25s %s",
 				"\tinstruction address", instructionAddressTemp);
@@ -284,14 +264,6 @@ public class InstructionDecodeStage implements Stage
 	@Override
 	public void reset()
 	{
-		ifCount = 0;
-		idCount = 0;
-		
-		hot = false;
-		
-		bubble = false;
-		nextBubble = false;
-		
 		currentExecuteStageState = null;
 		currentMemoryStageState = null;
 	}
