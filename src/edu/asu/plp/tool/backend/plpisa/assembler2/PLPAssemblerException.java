dbb26e2a1fefe3e921c8404d15d894cd5a13e777
dmcquarr <dmcquarr@asu.edu>
2017-02-13 16:09:12 -0700

    Changes
    Commit changes 2/13/17

---
+++ src/edu/asu/plp/tool/backend/plpisa/assembler2/PLPAssemblerException.java
@@ -0,0 +1,7 @@
+package edu.asu.plp.tool.backend.plpisa.assembler2;
+
+import edu.asu.plp.tool.backend.isa.exceptions.AssemblerException;
+
+public class PLPAssemblerException extends AssemblerException {
+
+}
