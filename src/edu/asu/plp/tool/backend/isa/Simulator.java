e6d30493a19bcfec1cc1a70f61a4ceae644bc7c8
Jacob Gersztyn <jabocg@gmail.com>
2017-03-22 23:03:22 -0700

    Add SimulatorException to Simulator.run
    And add a generic try/catch to Main.

--- src/edu/asu/plp/tool/backend/isa/Simulator.java
+++ src/edu/asu/plp/tool/backend/isa/Simulator.java
@@ -5,29 +5,29 @@ import edu.asu.plp.tool.backend.isa.exceptions.SimulatorException;
 public interface Simulator
 {
 	boolean isRunning();
-	
-	boolean run();
-	
+
+	boolean run() throws SimulatorException;
+
 	boolean step() throws SimulatorException;
-	
+
 	void reset();
-	
+
 	boolean isPaused();
-	
+
 	boolean pause();
-	
+
 	boolean isSimModeEnabled();
-	
+
 	boolean toggleSimMode();
-	
+
 	boolean isProgramLoaded();
-	
+
 	boolean loadProgram(ASMImage assembledImage);
-	
+
 	RegisterFile getRegisterFile();
-	
+
 	AddressBus getAddressBus();
-	
+
 	void setIRQ(long value);
 	void maskIRQ(long value);
 }
