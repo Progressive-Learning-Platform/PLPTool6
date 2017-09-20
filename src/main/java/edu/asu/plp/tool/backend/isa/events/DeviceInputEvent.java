1e4860be3ca506ea66996d14351cdd1377b8e84b
Prashanth Rk <prashanth@prashanths-air.mobile.asu.edu>
2017-09-20 13:18:01 -0700

    Documentation for DeviceInputEvent and DeviceOutputEvent

--- src/main/java/edu/asu/plp/tool/backend/isa/events/DeviceInputEvent.java
+++ src/main/java/edu/asu/plp/tool/backend/isa/events/DeviceInputEvent.java
@@ -1,5 +1,9 @@
 package edu.asu.plp.tool.backend.isa.events;
-
+/**
+ * This class contains the attributes and behaviors of the DeviceInputEvent. It basically gets
+ * the Device Name and the data that is sent as input to the device, which is initialized using
+ * the constructor, and returned using the getters.
+ */
 public class DeviceInputEvent {
 	
 	private String deviceName;
@@ -9,11 +13,18 @@ public class DeviceInputEvent {
 		this.deviceName = deviceName;
 		this.deviceData = deviceData;
 	}
-
+	/**
+ 	*
+ 	* @return current device name (in String)
+ 	*/
 	public String getDeviceName() {
 		return deviceName;
 	}
 	
+	/**
+	 *
+ 	* @return data associated with the corresponding device name
+ 	*/
 	public Object getDeviceData() {
 		return deviceData;
 	}
--- src/main/java/edu/asu/plp/tool/backend/isa/events/DeviceOutputEvent.java
+++ src/main/java/edu/asu/plp/tool/backend/isa/events/DeviceOutputEvent.java
@@ -1,5 +1,10 @@
 package edu.asu.plp.tool.backend.isa.events;
 
+/**
+ * This class contains the attributes and behaviors of the DeviceOutputEvent. It basically gets
+ * the Device Name and the device data which is sent to the OuptutEvent, initialized using the
+ * constructor, and returned using the getters.
+ */
 public class DeviceOutputEvent {
 
 	private String deviceName;
@@ -9,11 +14,19 @@ public class DeviceOutputEvent {
 		this.deviceName = deviceName;
 		this.deviceData = deviceData;
 	}
-
+	
+	/**
+ 	 *
+ 	 * @return current device name (in String)
+ 	 */
 	public String getDeviceName() {
 		return deviceName;
 	}
 	
+	/**
+ 	 *
+  	 * @return data associated with the corresponding device name
+ 	 */
 	public Object getDeviceData() {
 		return deviceData;
 	}
