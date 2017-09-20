64838d1627f73419e419d4752f4bb895d462b0bf
Shirish <shirish.chavan@asu.edu>
2017-09-20 13:03:22 -0700

    Adding docummentation for MemWatchRequestEvent and MemWatchResponseEvent

--- src/edu/asu/plp/tool/backend/isa/events/MemWatchRequestEvent.java
+++ src/edu/asu/plp/tool/backend/isa/events/MemWatchRequestEvent.java
@@ -1,13 +1,24 @@
 package edu.asu.plp.tool.backend.isa.events;
 
+/**
+ * This class defines the request object for Memory watch event
+ */
 public class MemWatchRequestEvent {
-	
+
 	private long memoryAddress;
 
+	/**
+	 * Constructor for Memory watch request object
+	 * @param memoryAddress
+	 */
 	public MemWatchRequestEvent(long memoryAddress) {
 		this.memoryAddress = memoryAddress;
 	}
 
+	/**
+	 * getter for address that is requested to be watched
+	 * @return
+	 */
 	public long getMemoryAddress() {
 		return memoryAddress;
 	}
--- src/edu/asu/plp/tool/backend/isa/events/MemWatchResponseEvent.java
+++ src/edu/asu/plp/tool/backend/isa/events/MemWatchResponseEvent.java
@@ -2,26 +2,47 @@ package edu.asu.plp.tool.backend.isa.events;
 
 import javafx.beans.property.LongProperty;
 
+/**
+ * This class defines the response object for Memory watch event
+ */
 public class MemWatchResponseEvent {
 	
 	private boolean success;
 	private long watchedAddress;
 	private LongProperty memObject;
 
+	/**
+	 * Constructor for Memory watch response object
+	 * @param success
+	 * @param watchedAddress
+	 * @param memObject
+	 */
 	public MemWatchResponseEvent(boolean success, long watchedAddress, LongProperty memObject) {
 		this.success = success;
 		this.watchedAddress = watchedAddress;
 		this.memObject = memObject;
 	}
 
+	/**
+	 * getter for success flag
+	 * @return
+	 */
 	public boolean isSuccess() {
 		return success;
 	}
-	
+
+	/**
+	 * getter for address that is watched
+	 * @return
+	 */
 	public long getWatchedAddress() {
 		return watchedAddress;
 	}
 
+	/**
+	 * getter for property of the memory at particular address
+	 * @return
+	 */
 	public LongProperty getMemObject() {
 		return memObject;
 	}
