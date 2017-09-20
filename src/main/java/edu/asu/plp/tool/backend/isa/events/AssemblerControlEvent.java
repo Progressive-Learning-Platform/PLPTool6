b03b35a281eece48a22f1b1efaa5ef1bc6566374
mukuljadhav <mxdnjadhav@gmail.com>
2017-09-20 12:52:11 -0700

    Documentation of AssemblerControlEvent and AssemblerResultEvent

--- src/main/java/edu/asu/plp/tool/backend/isa/events/AssemblerControlEvent.java
+++ src/main/java/edu/asu/plp/tool/backend/isa/events/AssemblerControlEvent.java
@@ -4,12 +4,23 @@ import java.util.List;
 
 import edu.asu.plp.tool.backend.isa.ASMFile;
 
+/**
+ ** This class is controls the assembler and manages the events
+ **/
 public class AssemblerControlEvent {
 	
 	private String command;
 	private String projectName;
 	private String projectType;
 	private List<ASMFile> assemblerFiles;
+
+     /**
+     * This is a constructor for which accepts Command, ProjectName, ProjectType and AssemblerFiles parameters
+     * @param {String} Command        - It take the command for the assembler
+     * @param {string} ProjectName    - The name of the project
+     * @param {string} ProjectType    - It was the project type
+     * @param {string} AssemblerFiles - List of assembler files for the processing
+     * **/
 
 	public AssemblerControlEvent(String command, String projectName, String projectType, List<ASMFile> assemblerFiles) {
 		this.command = command;
@@ -18,18 +29,30 @@ public class AssemblerControlEvent {
 		this.assemblerFiles = assemblerFiles;
 	}
 
+    /**
+    ** This is a function which return a command
+    **/
 	public String getCommand() {
 		return command;
 	}
 
+    /**
+     ** This method returns the name of the project
+    **/
 	public String getProjectName() {
 		return projectName;
 	}
 
+	/**
+	 ** This function returns the project type
+    **/
 	public String getProjectType() {
 		return projectType;
 	}
 
+    /**
+     ** This function returns the list of assembler files
+    **/
 	public List<ASMFile> getAssemblerFiles() {
 		return assemblerFiles;
 	}
--- src/main/java/edu/asu/plp/tool/backend/isa/events/AssemblerResultEvent.java
+++ src/main/java/edu/asu/plp/tool/backend/isa/events/AssemblerResultEvent.java
@@ -2,13 +2,24 @@ package edu.asu.plp.tool.backend.isa.events;
 
 import edu.asu.plp.tool.backend.isa.ASMImage;
 
+/**
+ ** This class is controls the assembler and manages the result
+ **/
 public class AssemblerResultEvent {
 
 	private boolean assembleSuccess;
 	private String errorMessage;
 	private String projectName;
 	private ASMImage asmImage;
-	
+
+	/**
+	 * This is a constructor for which accepts Success, ErrorMessage, ProjectName and ASMImage parameters
+	 * @param {String} Success        - It takes the assembler success parameter
+	 * @param {String} ErrorMessage   - It takes the error message parameter
+	 * @param {string} ProjectName    - It takes the name of the project
+	 * @param {string} ASMImage       - It takes the ASM Image
+	 * **/
+
 	public AssemblerResultEvent(boolean assembleSuccess, String errorMessage,
 			String projectName, ASMImage asmImage) {
 		this.assembleSuccess = assembleSuccess;
@@ -17,18 +28,30 @@ public class AssemblerResultEvent {
 		this.asmImage = asmImage;
 	}
 
+	/**
+	 ** This is a function which return assemble success
+	 **/
 	public boolean getAssembleSuccess() {
 		return assembleSuccess;
 	}
 
+    /**
+     ** This is a function which return error message to the screen
+     **/
 	public String getErrorMessage() {
 		return errorMessage;
 	}
-	
+
+    /**
+     ** This is a function which return the project name
+     **/
 	public String getProjectName() {
 		return projectName;
 	}
 
+    /**
+     ** This is a function which return ASM Image
+     **/
 	public ASMImage getAsmImage() {
 		return asmImage;
 	}
