45d65a18f389f6105aa8ed1521fa7f0143355d85
Tushar Pandit <tushar.pandit18@gmail.com>
2017-09-24 13:41:48 -0700

    Documentation
    Documentation
    RegWatchRequestEvent.java
    RegWatchResponseEvent.java

--- src/main/java/edu/asu/plp/tool/backend/isa/events/RegWatchRequestEvent.java
+++ src/main/java/edu/asu/plp/tool/backend/isa/events/RegWatchRequestEvent.java
@@ -1,13 +1,16 @@
 package edu.asu.plp.tool.backend.isa.events;
 
+// Class to watch register values
 public class RegWatchRequestEvent {
 
 	private String registerName;
 
+	// Constructor with registerName as a parameter
 	public RegWatchRequestEvent(String registerName) {
 		this.registerName = registerName;
 	}
 
+	// Function to get the register Name
 	public String getRegisterName() {
 		return registerName;
 	}
--- src/main/java/edu/asu/plp/tool/backend/isa/events/RegWatchResponseEvent.java
+++ src/main/java/edu/asu/plp/tool/backend/isa/events/RegWatchResponseEvent.java
@@ -4,30 +4,42 @@ import javafx.beans.property.LongProperty;
 
 public class RegWatchResponseEvent {
 
+	// Boolean Variable to store the status (True or False)
 	private boolean success;
+
+	// String variable to hold the registerID
 	private String registerID;
+
+	// String variable to hold the registerName
 	private String registerName;
+
+	//LongProperty class defines a Property wrapping a long value.
 	private LongProperty regObject;
 
+	// Constructor
 	public RegWatchResponseEvent(boolean success, String registerID, String registerName, LongProperty regObject) {
 		this.success = success;
 		this.registerID = registerID;
 		this.registerName = registerName;
 		this.regObject = regObject;
 	}
-	
+
+	// Function returns success if the RegWatchResponseEvent is a success
 	public boolean isSuccess() {
 		return success;
 	}
 
+	// Getter Function to return registerID
 	public String getRegisterID() {
 		return registerID;
 	}
 
+	// Getter Function to return RegisterName
 	public String getRegisterName() {
 		return registerName;
 	}
-	
+
+	// Getter Function to return RegisterObject
 	public LongProperty getRegObject() {
 		return regObject;
 	}
