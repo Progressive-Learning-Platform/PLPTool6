089905587857046418a6e2cf28734476ae32a095
Prashanth Rk <rprashanth16@gmail.com>
2017-09-26 14:05:26 -0700

    Review changes for DeviceInputEvent and DeviceOutputEvent

--- src/main/java/edu/asu/plp/tool/backend/isa/events/DeviceInputEvent.java
+++ src/main/java/edu/asu/plp/tool/backend/isa/events/DeviceInputEvent.java
@@ -1,30 +1,38 @@
 package edu.asu.plp.tool.backend.isa.events;
+
 /**
- * This class contains the attributes and behaviors of the DeviceInputEvent. It basically gets
- * the Device Name and the data that is sent as input to the device, which is initialized using
- * the constructor, and returned using the getters.
+ * This class offers the communication object(event) from front-end to back-end. The front-end sets
+ * the Device Name and the data that is sent as input to the back-end device module using
+ * the constructor, and the back-end module gets the data using the getters.
  */
 public class DeviceInputEvent {
 	
 	private String deviceName;
 	private Object deviceData;
 
+	/**
+	 * @brief A constructor to initialize the Device Name and the data sent as input to the back-end module
+	 * @param deviceName a device name for matching the backend device module name
+	 * @param deviceData data of the device sent as input to the back-end module
+	 * @note if the deviceName doesn't exist on the backend, this input event will be discarded.
+	 */
 	public DeviceInputEvent(String deviceName, Object deviceData) {
 		this.deviceName = deviceName;
 		this.deviceData = deviceData;
 	}
+
 	/**
- 	*
- 	* @return current device name (in String)
- 	*/
+	 * @brief This is a getter method used by the back-end module to get the Device Name sent by the front-end module
+	 * @return current device name (in String)
+	 */
 	public String getDeviceName() {
 		return deviceName;
 	}
-	
+
 	/**
-	 *
- 	* @return data associated with the corresponding device name
- 	*/
+	 * @brief This is a getter method used by the back-end module to get the Device Data sent by the front-end module
+	 * @return data associated with the corresponding device name
+	 */
 	public Object getDeviceData() {
 		return deviceData;
 	}
--- src/main/java/edu/asu/plp/tool/backend/isa/events/DeviceOutputEvent.java
+++ src/main/java/edu/asu/plp/tool/backend/isa/events/DeviceOutputEvent.java
@@ -1,33 +1,40 @@
 package edu.asu.plp.tool.backend.isa.events;
 
 /**
- * This class contains the attributes and behaviors of the DeviceOutputEvent. It basically gets
- * the Device Name and the device data which is sent to the OuptutEvent, initialized using the
- * constructor, and returned using the getters.
+ * This class offers the communication object(event) from back-end to front-end. The back-end sets
+ * the Device Name and the data that is sent as input to the front-end device module using
+ * the constructor, and the front-end module gets the data using the getters.
  */
 public class DeviceOutputEvent {
 
 	private String deviceName;
 	private Object deviceData;
 
+	/**
+     * @brief A constructor to initialize the Device Name and the data by back-end, sent as input to the front-end module
+     * @param deviceName a device name for matching the front-end device module name
+     * @param deviceData data of the device sent as input to the front-end module
+	 * @note if the deviceName doesn't exist on the frontend, this input event will be discarded.
+	 */
 	public DeviceOutputEvent(String deviceName, Object deviceData) {
 		this.deviceName = deviceName;
 		this.deviceData = deviceData;
 	}
-	
-	/**
- 	 *
- 	 * @return current device name (in String)
- 	 */
+
+    /**
+     * @brief This is a getter method used by the front-end module to get the Device Name, sent by the back-end module
+     * @return current device name (in String)
+     */
 	public String getDeviceName() {
 		return deviceName;
 	}
-	
-	/**
- 	 *
-  	 * @return data associated with the corresponding device name
- 	 */
+
+    /**
+     * @brief This is a getter method used by the front-end module to get the Device Data, sent by the back-end module
+     * @return data associated with the corresponding device name
+     */
 	public Object getDeviceData() {
 		return deviceData;
 	}
 }
+
