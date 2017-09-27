57b426723586d974d3cd53c78ef143a695e8e25e
Xiangyu Guo <tobielf@gmail.com>
2017-09-26 23:25:58 -0700

    Fixing indent issue.
    Unify the indent using Tab instead of spaces. I like using spaces
    though, but the whole project is based on Tab. We need to do a
    conversion later. Maybe in some file the tab and space is mixed.

--- src/main/java/edu/asu/plp/tool/backend/isa/events/DeviceOutputEvent.java
+++ src/main/java/edu/asu/plp/tool/backend/isa/events/DeviceOutputEvent.java
@@ -11,9 +11,9 @@ public class DeviceOutputEvent {
 	private Object deviceData;
 
 	/**
-     * @brief A constructor to initialize the Device Name and the data by back-end, sent as input to the front-end module
-     * @param deviceName a device name for matching the front-end device module name
-     * @param deviceData data of the device sent as input to the front-end module
+	 * @brief A constructor to initialize the Device Name and the data by back-end, sent as input to the front-end module
+	 * @param deviceName a device name for matching the front-end device module name
+	 * @param deviceData data of the device sent as input to the front-end module
 	 * @note if the deviceName doesn't exist on the frontend, this input event will be discarded.
 	 */
 	public DeviceOutputEvent(String deviceName, Object deviceData) {
@@ -21,18 +21,18 @@ public class DeviceOutputEvent {
 		this.deviceData = deviceData;
 	}
 
-    /**
-     * @brief This is a getter method used by the front-end module to get the Device Name, sent by the back-end module
-     * @return current device name (in String)
-     */
+	/**
+	 * @brief This is a getter method used by the front-end module to get the Device Name, sent by the back-end module
+	 * @return current device name (in String)
+	 */
 	public String getDeviceName() {
 		return deviceName;
 	}
 
-    /**
-     * @brief This is a getter method used by the front-end module to get the Device Data, sent by the back-end module
-     * @return data associated with the corresponding device name
-     */
+	/**
+	 * @brief This is a getter method used by the front-end module to get the Device Data, sent by the back-end module
+	 * @return data associated with the corresponding device name
+	 */
 	public Object getDeviceData() {
 		return deviceData;
 	}
