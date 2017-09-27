68b4b3e9cea1fc66e7e26df27539850a2d081eb0
Tushar Pandit <tushar.pandit18@gmail.com>
2017-09-26 23:07:22 -0700

    Comments

--- src/main/java/edu/asu/plp/tool/backend/isa/events/RegWatchRequestEvent.java
+++ src/main/java/edu/asu/plp/tool/backend/isa/events/RegWatchRequestEvent.java
@@ -5,6 +5,9 @@ package edu.asu.plp.tool.backend.isa.events;
  */
 public class RegWatchRequestEvent {
 
+	/**
+	 * String Variable to store the registerName
+	 */
 	private String registerName;
 
 	/**
