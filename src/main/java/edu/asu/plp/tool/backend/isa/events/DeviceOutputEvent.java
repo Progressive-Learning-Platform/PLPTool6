ef2e681dcd9c84a9b0f75b17b883f0b45a6a1e0d
Xiangyu Guo <tobielf@gmail.com>
2017-09-26 23:32:19 -0700

    Change a typo
    In the DeviceOutputEvent.java, the data is the output from back-end to
    the front-end, not the input.

--- src/main/java/edu/asu/plp/tool/backend/isa/events/DeviceOutputEvent.java
+++ src/main/java/edu/asu/plp/tool/backend/isa/events/DeviceOutputEvent.java
@@ -2,7 +2,7 @@ package edu.asu.plp.tool.backend.isa.events;
 
 /**
  * This class offers the communication object(event) from back-end to front-end. The back-end sets
- * the Device Name and the data that is sent as input to the front-end device module using
+ * the Device Name and the data that is sent as output to the front-end device module using
  * the constructor, and the front-end module gets the data using the getters.
  */
 public class DeviceOutputEvent {
@@ -11,10 +11,10 @@ public class DeviceOutputEvent {
 	private Object deviceData;
 
 	/**
-	 * @brief A constructor to initialize the Device Name and the data by back-end, sent as input to the front-end module
+	 * @brief A constructor to initialize the Device Name and the data by back-end, sent as output to the front-end module
 	 * @param deviceName a device name for matching the front-end device module name
-	 * @param deviceData data of the device sent as input to the front-end module
-	 * @note if the deviceName doesn't exist on the frontend, this input event will be discarded.
+	 * @param deviceData data of the device sent as output to the front-end module
+	 * @note if the deviceName doesn't exist on the frontend, this output event will be discarded.
 	 */
 	public DeviceOutputEvent(String deviceName, Object deviceData) {
 		this.deviceName = deviceName;
