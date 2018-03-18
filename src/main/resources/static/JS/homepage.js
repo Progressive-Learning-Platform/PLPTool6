0f570229d9bcfca9c92583f2494433ee46c8ba98
Tushar Pandit <tushar.pandit18@gmail.com>
2018-03-18 00:21:35 -0700

    Added sign out functionality
    Added sign out functionality

--- src/main/resources/static/JS/homepage.js
+++ src/main/resources/static/JS/homepage.js
@@ -4,7 +4,8 @@
          $("#profileImage").attr("src",localStorage.ImageURL);
             var rss = (function ($) {
                 var ht = $(window).height()*0.82;
-                console.log("ht is"+ht);
+//                console.log("ht is"+ht);
+                console.log(localStorage);
                 var createWidgets = function () {
                     $('#mainSplitter').jqxSplitter({ width: '100%', height: ht, orientation: 'horizontal', panels: [{ size: '80%', collapsible: false }] });
                     $('#firstNested').jqxSplitter({ width: '100%', height: '100%',  orientation: 'vertical', panels: [{ size: '12.5%', collapsible: false}] });
@@ -25,14 +26,43 @@
 
         });
 
-        function signOut() {
-            var auth2 = gapi.auth2.getAuthInstance();
-            auth2.signOut().then(function () {
-            console.log('User signed out.');
-            window.location.href = 'index.html';
+    function signOut() {
 
-            });
-        }
+    }
+
+
+// Log out menu
+
+function dropDownToggle() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+    }
+
+function logout(){
+    var auth2 = gapi.auth2.getAuthInstance();
+
+    auth2.signOut().then(function () {
+    console.log('User signed out.');
+    window.location.href = 'index.html';
+    });
+
+}
+
+
 
 
 
